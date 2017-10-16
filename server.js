b02ce6a607d98b365dff7534437175b8e7bff632
Marina <dubovik.m14@gmail.com>
2017-10-16 11:18:30 +0200

    Little modifications

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const logger = fs.createWriteStream('client_id.log');
 let seed = 0;
 
 const server = net.createServer((client) => {
-  console.log('\nClient connected');
+  console.log('Client connected');
   client.setEncoding('utf8');
 
   client.on('data', (data, err) =>
@@ -17,26 +17,22 @@ const server = net.createServer((client) => {
       else if (!err && data === clientString)
       {
           client.id = Date.now() + seed++;
-          writeLog('Client #' + client.id + ' connected\n');
+          logger.write('Client #' + client.id + ' connected\n');
           client.write(data === clientString ? good : bad);
       }
       else if (!err && data !== clientString) {
-          writeLog('Client #' + client.id + ' has asked: ' + data + '\n');
+          logger.write('Client #' + client.id + ' has asked: ' + data + '\n');
           let answer = generateAnswer();
-          writeLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
+          logger.write('Server answered to Client #' + client.id + ': ' + answer + '\n');
           client.write(answer);
       }
   });
   client.on('end', () =>
   {
       logger.write('Client #'+ client.id+ ' disconnected');
-      console.log('Client disconnected')
+      console.log('Client disconnected\n')
   });
 });
-
-function writeLog(data) {
-  logger.write(data);
-}
 
 function generateAnswer(){
   return Math.random() > 0.5 ? '1' : '0';

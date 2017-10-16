66418017e2b612cfd5d8e107718c4c325a9da204
Marina <dubovik.m14@gmail.com>
2017-10-16 23:22:10 +0200

    Final changes

--- client-swarm.js
+++ client-swarm.js
@@ -1,13 +1,10 @@
 let N = process.argv[2];
-const exec = require('child_process').exec;
+const child_process = require('child_process');
 
 for ( ;N > 0; N--) {
-    console.log(`number:\n ${N}`);
-    exec('node client.js', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`________________________:\n ${stdout}`);
+    console.log(`number: ${N}`);
+    child_process.exec('node client.js', (error, stdout, stderr) => {
+        if (error) console.error(`exec error: ${error}`);
+        console.log(`----------------------------------\n${stdout}`);
     });
 }
--- server.js
+++ server.js
@@ -4,40 +4,40 @@ const port = 8124;
 const clientString = 'QA';
 const good = 'ACK';
 const bad = 'DEC';
-const logger = fs.createWriteStream('client_id.log');
+const log = fs.createWriteStream('client_id.log');
 let seed = 0;
 
 const server = net.createServer((client) => {
   console.log('Client connected');
   client.setEncoding('utf8');
 
-  client.on('data', (data, err) =>
-  {
-      if (err) console.error(err);
-      else if (!err && data === clientString)
-      {
-          client.id = Date.now() + seed++;
-          logger.write('Client #' + client.id + ' connected\n');
-          client.write(data === clientString ? good : bad);
-      }
-      else if (!err && data !== clientString) {
-          logger.write('Client #' + client.id + ' has asked: ' + data + '\n');
-          let answer = generateAnswer();
-          logger.write('Server answered to Client #' + client.id + ': ' + answer + '\n');
-          client.write(answer);
-      }
-  });
+  client.on('data', (data, err) => {
+    if (err) console.error("Ошибка получения информации от клиента");
+    else if (data === clientString) {
+      client.id = Date.now() + seed++;
+      log.write('Client #' + client.id + ' connected\n');
+      client.write(data === clientString ? good : bad);
+    }
+    else if (data !== clientString) {
+      log.write('Client #' + client.id + ' has asked: ' + data + '\n');
+      let answer = generateAnswer();
+      log.write('Server answered to Client #' + client.id + ': ' + answer + '\n');
+      client.write(answer);
+    }
+});
+
   client.on('end', () =>
   {
-      logger.write('Client #'+ client.id+ ' disconnected');
-      console.log('Client disconnected\n')
+    log.write('Client #'+ client.id+ ' disconnected\n');
+    console.log('Client disconnected\n')
   });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on localhost: ${port}\n`);
 });
 
 function generateAnswer(){
   return Math.random() > 0.5 ? '1' : '0';
 }
 
-server.listen(port, () => {
-  console.log(`Server listening on localhost: ${port}`);
-});
